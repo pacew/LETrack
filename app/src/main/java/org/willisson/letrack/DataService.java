97b3943e064505abb154f3df7b28fa48400ce0dc
Pace Willisson <pace@alum.mit.edu>
2015-11-28 14:43:38 -0500

    location

--- app/src/main/java/org/willisson/letrack/DataService.java
+++ app/src/main/java/org/willisson/letrack/DataService.java
@@ -2,12 +2,22 @@ package org.willisson.letrack;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
 import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by pace on 11/28/15.
  */
-public class DataService extends IntentService {
+public class DataService extends IntentService
+        implements LocationListener, GoogleApiClient.ConnectionCallbacks,
+                GoogleApiClient.OnConnectionFailedListener {
     public final String TAG = "LEtrack";
     static boolean running;
 
@@ -24,6 +34,8 @@ public class DataService extends IntentService {
         Log.i(TAG, "starting data service");
         running = true;
 
+        loc_test();
+
         while (true) {
             get_datapoint();
             int interval = 10; /* seconds */
@@ -35,6 +47,50 @@ public class DataService extends IntentService {
             }
         }
 
+    }
+
+    GoogleApiClient mGoogleApiClient;
+
+    void loc_test () {
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    public void onConnected (Bundle bundle) {
+        Log.i(TAG, "onconnected");
+
+        LocationRequest req = new LocationRequest();
+        req.setInterval (10 * 1000);
+        req.setFastestInterval (10 * 1000);
+        req.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+
+
+        LocationServices.FusedLocationApi.requestLocationUpdates (mGoogleApiClient, req, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended (int i) {
+        Log.i (TAG, "connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed: " + connectionResult.toString());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i (TAG, "location changed " + location);
     }
 
     void get_datapoint() {

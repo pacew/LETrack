45e1eae4d5f070ecccbfc58e7310d001708c7a50
Alex Willisson <atw@alum.mit.edu>
2015-11-28 12:18:47 -0500

    play services working

--- app/src/main/java/org/willisson/letrack/MainActivity.java
+++ app/src/main/java/org/willisson/letrack/MainActivity.java
@@ -2,6 +2,10 @@ package org.willisson.letrack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import android.content.Context;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(getApplicationContext()) != ConnectionResult.SUCCESS) {
+			send_toast("Google Play Services not found.");
+			finish();
+		}
     }
+
+    public void send_toast (String text) {
+		Context context = getApplicationContext ();
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText (context, text, duration);
+		toast.show();
+    }
+
 }
